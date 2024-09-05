5efd4396ba59d630ca1580af7f4fb12989e93470
AndroidSoftwares <95849029+AndroidSoftwares@users.noreply.github.com>
2024-09-05 09:16:49 +0800

    Update gobind.sh

--- alist-lib/scripts/gobind.sh
+++ alist-lib/scripts/gobind.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 cd ../alistlib || exit
-if [ "$1" == "debug" ]; then
-  gomobile bind -ldflags "-s -w" -v -androidapi 19 -target="android/arm64"
-else
-  gomobile bind -ldflags "-s -w" -v -androidapi 19
-fi
+# if [ "$1" == "debug" ]; then
+#   gomobile bind -ldflags "-s -w" -v -androidapi 19 -target="android/arm64"
+# else
+#   gomobile bind -ldflags "-s -w" -v -androidapi 19
+# fi
+gomobile bind -ldflags "-s -w" -v -androidapi 19 -target="android/arm64"
 
 echo "Moving aar and jar files to android/app/libs"
 mkdir -p ../../android/app/libs
